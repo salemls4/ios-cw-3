0dfc5d4741f4129e87a69965771be9cbb9b2c8ec
salemls4 <84942947+salemls4@users.noreply.github.com>
2022-03-05 12:32:37 +0300

    done

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -8,10 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+    var body: some View
+    {
+        
+       VStack{
+           Text("hello my name is salem")
+           
+           
+           HStack {
+               VStack {
+
+           
+           Image("kc")
+               .resizable()
+               .frame(width: 130, height: 130)
+                   Text("MARVEL")
+               }
+               VStack {
+           Image("dc")
+               .resizable()
+               .frame(width: 130, height: 130)
+                   Text("marvel")}
+           }
+          HStack
+           {
+               VStack {
+               Image("fc")
+               .resizable()
+               .frame(width: 130, height: 130)
+               Text ("marvel")
+               }
+               VStack{
+           Image("sc")
+               .resizable()
+               .frame(width: 130, height: 130)
+                   Text("marvel")
+                   
+               }
+           }
+            VStack {
+           Image("ac")
+               .resizable()
+               .frame(width: 130, height: 130)
+                   Text("marvel")
+               }
+               
             .padding()
-    }
+           
+        }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
